5599a5c357e6efd7da01bc3bcd74f42df616e109
Kerem Havlucu <keremhvlc@gmail.com>
2025-10-12 16:00:13 +0300

    feat (application) : delete meeting participant

---
+++ Src/Core/MeetingManagementSystem.Application/Features/MeetingParticipantFeatures/Commands/DeleteMeetingParticipantCommand/DeleteMeetingParticipantCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MeetingManagementSystem.Domain.Dtos;
+
+namespace MeetingManagementSystem.Application.Features.MeetingParticipantFeatures.Commands.DeleteMeetingParticipantCommand
+{
+    public class DeleteMeetingParticipantCommand : IRequest<MessageResponse>
+    {
+    }
+}
---
+++ Src/Core/MeetingManagementSystem.Application/Features/MeetingParticipantFeatures/Commands/DeleteMeetingParticipantCommand/DeleteMeetingParticipantCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using MeetingManagementSystem.Domain.Dtos;
+
+namespace MeetingManagementSystem.Application.Features.MeetingParticipantFeatures.Commands.DeleteMeetingParticipantCommand
+{
+    public class DeleteMeetingParticipantCommandHandler : IRequestHandler<DeleteMeetingParticipantCommand, MessageResponse>
+    {
+        public Task<MessageResponse> Handle(DeleteMeetingParticipantCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
