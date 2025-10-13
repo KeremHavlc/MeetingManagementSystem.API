0cedc04749abb7956771c576e2418d4e2c457e29
Kerem Havlucu <keremhvlc@gmail.com>
2025-10-13 03:01:44 +0300

    feat (auth) : authorize added

---
+++ Src/External/MeetingManagementSystem.Infrastructure/Security/Authorization/MeetingRoleHandler.cs
@@ -0,0 +1,31 @@
+﻿using MeetingManagementSystem.Domain.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace MeetingManagementSystem.Infrastructure.Security.Authorization
+{
+    public class MeetingRoleHandler : AuthorizationHandler<MeetingRoleRequirement>
+    {
+        private readonly IMeetingParticipantRepository _meetingParticipantRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MeetingRoleHandler(IHttpContextAccessor httpContextAccessor, IMeetingParticipantRepository meetingParticipantRepository)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _meetingParticipantRepository = meetingParticipantRepository;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, MeetingRoleRequirement requirement)
+        {
+            //JWT'den token al
+            var userIdClaim = _httpContextAccessor.HttpContext?.User?.FindFirst("id").Value;
+            if (userIdClaim == null) return;
+
+            var userId = Guid.Parse(userIdClaim);
+
+            //Routedan meetingId Al
+            //var meetingId = _httpContextAccessor.HttpContext?.Request.RouteValues
+
+        }
+    }
+}
---
+++ Src/External/MeetingManagementSystem.Infrastructure/Security/Authorization/MeetingRoleRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace MeetingManagementSystem.Infrastructure.Security.Authorization
+{
+    public class MeetingRoleRequirement : IAuthorizationRequirement
+    {
+        public string RequiredRole { get; }
+
+        public MeetingRoleRequirement(string requiredRole)
+        {
+            RequiredRole = requiredRole;
+        }
+    }
+}
