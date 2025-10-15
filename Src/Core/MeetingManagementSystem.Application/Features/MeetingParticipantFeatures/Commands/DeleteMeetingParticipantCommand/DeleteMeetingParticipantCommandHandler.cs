e1ad7272ec80708126e2b6ea431c2ea6323231cb
Kerem Havlucu <keremhvlc@gmail.com>
2025-10-15 14:37:59 +0300

    feat(application): implement delete meeting participant command handler

--- Src/Core/MeetingManagementSystem.Application/Features/MeetingParticipantFeatures/Commands/DeleteMeetingParticipantCommand/DeleteMeetingParticipantCommandHandler.cs
+++ Src/Core/MeetingManagementSystem.Application/Features/MeetingParticipantFeatures/Commands/DeleteMeetingParticipantCommand/DeleteMeetingParticipantCommandHandler.cs
@@ -2,52 +2,75 @@
 using MeetingManagementSystem.Domain.Dtos;
 using MeetingManagementSystem.Domain.Entities;
 using MeetingManagementSystem.Domain.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 
 namespace MeetingManagementSystem.Application.Features.MeetingParticipantFeatures.Commands.DeleteMeetingParticipantCommand
 {
     public class DeleteMeetingParticipantCommandHandler : IRequestHandler<DeleteMeetingParticipantCommand, MessageResponse>
     {
+
+        private readonly IMeetingParticipantRepository _meetingParticipantRepository;
         private readonly IMeetingRepository _meetingRepository;
+        private readonly IMeetingRoleRepository _meetingRoleRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly UserManager<AppUser> _userManager;
 
-        public DeleteMeetingParticipantCommandHandler(IMeetingRepository meetingRepository, UserManager<AppUser> userManager)
+        public DeleteMeetingParticipantCommandHandler(UserManager<AppUser> userManager, IHttpContextAccessor httpContextAccessor, IMeetingRoleRepository meetingRoleRepository, IMeetingRepository meetingRepository, IMeetingParticipantRepository meetingParticipantRepository)
         {
-            _meetingRepository = meetingRepository;
             _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
+            _meetingRoleRepository = meetingRoleRepository;
+            _meetingRepository = meetingRepository;
+            _meetingParticipantRepository = meetingParticipantRepository;
         }
-
-
 
         public async Task<MessageResponse> Handle(DeleteMeetingParticipantCommand request, CancellationToken cancellationToken)
         {
-            if (!Guid.TryParse(request.UserId, out Guid userId))
+            if(!Guid.TryParse(request.DeleteUserId , out Guid deleteUserId))
             {
                 return new MessageResponse
                 {
-                    Message = "Hatalı userId formatı!",
+                    Message = "Geçersiz userId formatı",
                     Success = false
                 };
             }
-            var existUser = await _userManager.FindByIdAsync(request.UserId);
-            if (existUser == null)
+            if(!Guid.TryParse(request.MeetingId , out Guid meetingId))
             {
                 return new MessageResponse
                 {
-                    Message = "Kullanıcı bulunamadı!",
+                    Message = "Geçersiz meetingId formatı",
                     Success = false
                 };
             }
-            if (!Guid.TryParse(request.MeetingId, out Guid meetingId))
+            var currentUserIdClaim = _httpContextAccessor.HttpContext?.User?.FindFirst("Id")?.Value;
+            if (string.IsNullOrEmpty(currentUserIdClaim))
             {
                 return new MessageResponse
                 {
-                    Message = "Hatalı meetingId formatı!",
+                    Message = "Yetkisiz kullanıcı!",
                     Success = false
                 };
             }
-            var existMeeting = await _meetingRepository.GetByIdAsync(meetingId);
-            if (existMeeting == null)
+            if(!Guid.TryParse(currentUserIdClaim , out Guid currentUserId))
+            {
+                return new MessageResponse
+                {
+                    Message = "Geçersizk userId formatı",
+                    Success = false
+                };
+            }
+            var existDeleteUser = await _userManager.FindByIdAsync(request.DeleteUserId);
+            if(existDeleteUser == null)
+            {
+                return new MessageResponse
+                {
+                    Message = "Silinmek istenen kullanıcı bulunamadı!",
+                    Success = false
+                };
+            }
+            var existMeeting = await _meetingRepository.GetSingleAsync(mr => mr.Id == meetingId);
+            if(existMeeting == null)
             {
                 return new MessageResponse
                 {
@@ -55,7 +78,26 @@ namespace MeetingManagementSystem.Application.Features.MeetingParticipantFeature
                     Success = false
                 };
             }
-            throw new Exception();
+
+            var currentParticipant = (await _meetingParticipantRepository.GetWhereAsync(mp => mp.MeetingId == meetingId && mp.UserId == currentUserId)).FirstOrDefault();
+            if(currentParticipant == null)
+            {
+                return new MessageResponse
+                {
+                    Message = "Toplantıya katılımcı değilsiniz!",
+                    Success = false
+                };
+            }
+
+            var meetingRole = await _meetingRoleRepository.GetByIdAsync(currentParticipant.RoleId);
+            if(meetingRole == null)
+            {
+                return new MessageResponse
+                {
+                    Message = "Rol bilgisi bulunamadı!",
+                    Success = false
+                };
+            }
         }
     }
 }
